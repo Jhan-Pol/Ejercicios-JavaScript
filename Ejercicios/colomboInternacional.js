d769ec53437a4259263a92e828288baf61099dea
Jhan Pol Giraldo <jhanpool911@gmail.com>
2024-01-01 20:58:30 -0500

    Inclusion sobre Ejercicios de la u colombo

---
+++ Ejercicios/colomboInternacional.js
@@ -0,0 +1,52 @@
+//Programa para determinar si un número dado por el usuario es positivo o negativo
+// let numero = parseInt(prompt("Digite un numero"));
+// verificar(numero);
+// function verificar(numero) {
+//     if(numero>=0) {
+//         console.log("El numero introducido es positivo :)");
+//     }
+//     else {
+//         console.log("El numero introducido es negativo");
+//     }
+// }
+//-------------------------------------------------------------------------------------------
+//Programa que calcule el resto de cualquier división entera
+// let numero = parseInt(prompt("Digite el numero a dividir"));
+// let divisor = parseInt(prompt("Digite el divisor"));
+// division(numero, divisor);
+// function division(numero, divisor) {
+//     let resto =numero % divisor;
+//     console.log("El resto de la division es: " + resto);
+// }
+//--------------------------------------------------------------------------------------------
+//Introducir tres números por teclado, hallar el promedio de los tres números, la suma y desplegar del primer 
+//número el doble, del segundo número el triple y del tercer número el cuadrado
+// let num1 = parseFloat(prompt("Introduzca el primer numero"));
+// let num2 = parseFloat(prompt("Introduzca el segundo numero"));
+// let num3 = parseFloat(prompt("Introduzca el tercer numero"));
+// promedio(num1, num2, num3);
+// function promedio(num1, num2, num3) {
+//     let promedio = parseFloat((num1 + num2 + num3) / 3);
+//     console.log(`El promedio de los tres numeros es: ${promedio}`);
+// }
+//---------------------------------------------------------------------------------------------
+//Introducir tres números por teclado mostrar los últimos dígitos de cada número.
+// let num1 = prompt("Digite el primer numero");
+// let num2 = prompt("Digite el segundo numero");
+// let num3 = prompt("Digite el tercer numero");
+// traspaso(num1);
+// traspaso(num2);
+// traspaso(num3);
+// function traspaso(variable) {
+//     var traspaso = variable.toString();
+//     var tamaño = traspaso.length;
+//     for(let i = 0; i < tamaño; i++) {
+//         console.log(traspaso[tamaño - 1]);
+//     }
+// }
+//------------------------------------------------------------------------------------------------
+
+
+
+
+
