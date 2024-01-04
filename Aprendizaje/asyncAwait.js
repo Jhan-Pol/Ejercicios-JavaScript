3214e01934b5afab514650334b753b2d9bafeb13
Jhan Pol Giraldo <jhanpool911@gmail.com>
2024-01-04 00:32:14 -0500

    Inclusion de async y await

--- Aprendizaje/asyncAwait.js
+++ Aprendizaje/asyncAwait.js
@@ -11,12 +11,27 @@ const fetchPosts = () => {
         },2000);
     });
 };
-console.log("Comienza operacion");
-fetchPosts()
-    .then((posts) => {
+//De esta manera vemos el tiempo asincronico, es decir que la promesa se ejecuta por debajo del otro codigo 
+// console.log("Comienza operacion");
+// fetchPosts()
+//     .then((posts) => {
+//         console.log(posts);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+// console.log("Finaliza operacion");
+//Si queremos que se ejecute primero la promesa y luego el demas codigo tendremos que hacer uso de async y await
+const mostrarPosts = async() => {
+    try {
+        const posts = await fetchPosts();
+        //todo el codigo que pongamos por
+        //debajo de la promesa se ejecutara
+        //despues del resultado que presente esta
         console.log(posts);
-    })
-    .catch((error) => {
+    } catch(error) {
         console.log(error);
-    });
-console.log("Finaliza operacion");
+    }
+    
+}
+mostrarPosts();
